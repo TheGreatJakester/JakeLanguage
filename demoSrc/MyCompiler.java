598e618f622c5024dafeb1579c0c1a6435b1ba6a
Jacob Moulton <jacobmoulton@SUU.EDU>
2019-09-24 10:25:18 -0600

    initial commit

---
+++ demoSrc/MyCompiler.java
@@ -0,0 +1,21 @@
+/*
+ * This is where you run your compiler.
+ * you will need to provide your "code" file to the compiler as a string to your lexer
+ * you can create any data structure that will contain your Tokens. One way is to use linked lists
+ * */
+
+
+
+public class MyCompiler {
+
+	public static void main(String[] args) {
+		// you need to read the input string from your code and extract the tokens here.
+		
+
+	}
+	
+	
+
+	
+
+}
