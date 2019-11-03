845b39ece48e28615ce26415b0c692481c8e84fc
Jacob Moulton <Jacob.Moulton@gmail.com>
2019-11-03 13:21:12 -0700

    add Operator

---
+++ Operator.py
@@ -0,0 +1,10 @@
+
+
+class Operator:
+    def evaluate(operand1,operand):
+        pass
+    self.name = ""
+
+    def doubleCheck(operand1,operand2,data_type):
+        return operand1.type == data_type and operand2.type == data_type:
+            
